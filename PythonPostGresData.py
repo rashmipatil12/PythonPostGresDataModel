9c16fae3c97a0ac0de3a0c468ffee5b68ddedb75
rashmipati12 <06rashmi09patil@gmail.com>
2023-01-12 15:31:31 +0530

    some changes added

--- PythonPostGresData.py
+++ PythonPostGresData.py
@@ -3,13 +3,13 @@ import psycopg2
 try: 
     conn = psycopg2.connect("host=127.0.0.1 dbname=postgres user=postgres password=root")
 except psycopg2.Error as e: 
-    print("Error: Could not make connection to the Postgres database")
+    print("Error in connecting to the Postgres database")
     print(e)
 
 try: 
     cur = conn.cursor()
 except psycopg2.Error as e: 
-    print("Error: Could not get curser to the Database")
+    print("Error: Could not get curser to the DB")
     print(e)    
 
 conn.set_session(autocommit=True)
@@ -49,7 +49,7 @@ except psycopg2.Error as e:
 try: 
     cur.execute("INSERT INTO students (student_id, name, age, gender, subject, marks) \
                  VALUES (%s, %s, %s, %s, %s, %s)", \
-                 (1, "Raj", 23, "Male", "Python", 85))
+                 (1, "Rashmi", 25, "Female", "C++", 85))
 except psycopg2.Error as e: 
     print("Error: Inserting Rows")
     print (e)
@@ -57,7 +57,7 @@ except psycopg2.Error as e:
 try: 
     cur.execute("INSERT INTO students (student_id, name, age, gender, subject, marks) \
                   VALUES (%s, %s, %s, %s, %s, %s)",
-                  ( 2, "Priya", 22, "Female", "Python", 86))
+                  ( 2, "Ram", 22, "Female", "JAVA", 76))
 except psycopg2.Error as e: 
     print("Error: Inserting Rows")
     print (e)
